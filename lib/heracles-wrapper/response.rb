af35e79302f0ea7d296bcea7dee1c110839f433a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-05 10:31:32 -0500

    Updating tests for Ruby 1.8.7 compatability

--- lib/heracles-wrapper/response.rb
+++ lib/heracles-wrapper/response.rb
@@ -16,8 +16,8 @@ if __FILE__ == $0
   require 'minitest/autorun'
   describe 'Heracles::Wrapper::Response' do
     subject { Heracles::Wrapper::Response.new(params) }
-    let(:expected_request_parameters) { { callback_url: 'http://google.com' }}
-    let(:params) { {request: expected_request_parameters } }
+    let(:expected_request_parameters) { { callback_url => 'http://google.com' }}
+    let(:params) { {request => expected_request_parameters } }
     it 'should have #request_parameters' do
       subject.request_parameters.must_equal expected_request_parameters
     end
