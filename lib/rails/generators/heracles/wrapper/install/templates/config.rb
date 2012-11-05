ea0127cbe7a4a92dc094296e788a454cfec2b30e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-05 11:41:45 -0500

    Updating generator to account for API by env

--- lib/rails/generators/heracles/wrapper/install/templates/config.rb
+++ lib/rails/generators/heracles/wrapper/install/templates/config.rb
@@ -1,3 +1,15 @@
 Heracles::Wrapper.configure do |config|
-  config.api_key = "<%= options[:api_key] %>"
+  if Rails.env.production?
+    config.api_key = "<%= options[:api_key] %>"
+  end
+
+  if Rails.env.development?
+    config.api_key = "change-me-as-needed"
+    config.heracles_base_url = 'http://localhost:8765'
+  end
+
+  if Rails.env.preproduction?
+    config.api_key = "change-me-as-needed"
+    config.heracles_base_url = 'https://heraclespprd.library.nd.edu'
+  end
 end
