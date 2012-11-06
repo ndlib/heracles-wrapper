6465f395168b7c51cec1509a01c28be2246ad7c9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-06 14:39:17 -0500

    Adding notification response spec

--- lib/rails/generators/heracles/wrapper/notification_response/notification_response_generator.rb
+++ lib/rails/generators/heracles/wrapper/notification_response/notification_response_generator.rb
@@ -10,6 +10,12 @@ module Heracles
           File.join('app/models/', "#{file_name}.rb")
         )
       end
+      def create_spec
+        template(
+          'notification_response_spec.rb.erb',
+          File.join('spec/models/',"#{file_name}_spec.rb")
+        )
+      end
     end
   end
 end
--- spec/rails/generators/heracles/wrapper/notification_response/notification_response_generator_spec.rb
+++ spec/rails/generators/heracles/wrapper/notification_response/notification_response_generator_spec.rb
@@ -1,17 +1,20 @@
 gem 'rails'
+TO_GEM_ROOT = "../../../../../../../"
 require File.expand_path(
   File.join(
-    '../../../../../../../lib/rails/generators/heracles/wrapper',
+    TO_GEM_ROOT, 'lib/rails/generators/heracles/wrapper',
     '/notification_response/notification_response_generator'
   ), __FILE__
 )
 module Heracles::Wrapper
   class NotificationResponseGeneratorTest < Rails::Generators::TestCase
     tests Heracles::Wrapper::NotificationResponseGenerator
-    destination File.expand_path("../tmp", File.dirname(__FILE__))
+    destination(
+      File.expand_path(File.join(TO_GEM_ROOT,"tmp", File.dirname(__FILE__)))
+    )
     setup :prepare_destination
 
-    test 'should generate with a name' do
+    test 'should generate model with a name' do
       run_generator(%w(MonkeyPatch))
       assert_file 'app/models/monkey_patch.rb' do |file|
         assert_match(
@@ -27,6 +30,14 @@ module Heracles::Wrapper
           file
         )
         assert_method(:initialize, file)
+      end
+    end
+
+    test 'should generate spec with a name' do
+      run_generator(%w(MonkeyPatch))
+      assert_file 'spec/models/monkey_patch_spec.rb' do |file|
+        assert_match(/^require 'spec_helper'/, file)
+        assert_match(/^describe MonkeyPatch do$/, file)
       end
     end
   end
