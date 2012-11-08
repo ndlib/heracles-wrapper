77f4d514410fb00bf2b224815f5e53dbcfce7d0d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-11-08 10:34:04 -0500

    Updating API generation

--- lib/rails/generators/heracles/wrapper/install/templates/config.rb
+++ lib/rails/generators/heracles/wrapper/install/templates/config.rb
@@ -1,15 +1,11 @@
 Heracles::Wrapper.configure do |config|
   if Rails.env.production?
     config.api_key = "<%= options[:api_key] %>"
-  end
-
-  if Rails.env.development?
-    config.api_key = "change-me-as-needed"
-    config.heracles_base_url = 'http://localhost:8765'
-  end
-
-  if Rails.env.preproduction?
+  elsif Rails.env.preproduction?
     config.api_key = "change-me-as-needed"
     config.heracles_base_url = 'https://heraclespprd.library.nd.edu'
+  else
+    config.api_key = "change-me-as-needed"
+    config.heracles_base_url = 'http://localhost:8765'
   end
 end
